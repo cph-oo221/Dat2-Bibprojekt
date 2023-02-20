b25553e9c662c4d540bd05ff511bbaa00f4fec02
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-20 10:26:54 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println(getString("Hvad er dit navn?"));
+
+        System.out.println(getInt("Angiv venligst din skostørrelse?"));
+
+        System.out.println(getInt("Angiv venligst din højde?"));
     }
 
     public static String getString(String s)
@@ -13,6 +17,27 @@ public class Main
 
         System.out.println(s + " : ");
 
+
         return scanner.nextLine();
     }
+
+    public static int getInt(String s)
+    {
+        int res = 0;
+
+
+        while (true)
+        {
+            try
+            {
+                res = Integer.parseInt(getString(s));
+                return res;
+            }
+            catch (Exception e)
+            {
+                System.out.println("Husk ikke tal ord");
+            }
+        }
+    }
+
 }
