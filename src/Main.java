6c5a4f4ff6277af1c6306bfb20763a35a1586105
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-20 10:06:15 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        System.out.println("Hej " + getString("Hvad er dit navn?") + "!");
     }
 
     public static String getString(String s)
