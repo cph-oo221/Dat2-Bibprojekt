0ee595850cd1796a70f85f9a5300a327ebb41bed
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-20 09:40:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static String getString(String s)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(s + " : ");
+
+        return scanner.nextLine();
+    }
+}
