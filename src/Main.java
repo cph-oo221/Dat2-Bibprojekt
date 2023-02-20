ce55ccfdff25ad23ebf603b95c9b929e8d844701
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-20 10:44:32 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,26 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println(getString("Hvad er dit navn?"));
+        /*System.out.println(getString("Hvad er dit navn?"));
 
         System.out.println(getInt("Angiv venligst din skostørrelse?"));
 
-        System.out.println(getInt("Angiv venligst din højde?"));
+        System.out.println(getInt("Angiv venligst din højde?"));*/
+
+        while (true)
+        {
+            switch (getString("angiv dit ønske ?"))
+            {
+                case "lån bog":
+                    System.out.println("du ønsker at låne en bog");
+                    break;
+                case "stop":
+                    break;
+            }
+            if(getString("q for quit").equalsIgnoreCase("q"))
+                break;
+        }
+
     }
 
     public static String getString(String s)
