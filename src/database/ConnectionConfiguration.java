474ff31da099216018183e5ae5765acf61f997ef
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-20 11:12:37 +0100

    update

---
+++ src/database/ConnectionConfiguration.java
@@ -0,0 +1,29 @@
+package database;
+
+import javax.swing.plaf.TextUI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConnectionConfiguration
+{
+    public static Connection getConnection()
+    {
+
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/bibliotek?serverTimezone=CET&useSSL=false";
+        String user = "root";
+        String password = "oo123";
+
+
+        try
+        {
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
