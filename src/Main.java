16b181a4380f337e7c1c73a1f3fed7db977b0343
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-02-22 17:56:44 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,18 @@
+import Entities.Bog;
 import Language.DanskDialog;
 import Language.Dialog;
 import Language.EngelskDialog;
+import Mapper.BogFacade;
+import Menu.Menu;
 import Utilities.Input;
+
+
 
 public class Main
 {
     public static void main(String[] args)
     {
-        /*System.out.println("Bøgerne er: ");
-        List<Bog> bogList = BogFacade.hentBøger();
-        for (Bog bog : bogList)
-        {
-            System.out.println(bog.toString());
-        }
-
-        System.out.println();
-        System.out.println("Bruger er:  ");
-        BrugerFacade.hentBruger();
-
-        System.out.println();
-
-        System.out.println("Forfattere er: ");
-        ForfatterFacade.hentForfatter();
-
-        System.out.println();
-
-        System.out.println("Zip code er: ");
-        PostnrFacade.hentPostnr();
-
-        System.out.println();
-
-        System.out.println("Udlaan er: ");
-        List<Udlån> udlånList = UdlånFacade.hentUdlån();
-        for (Udlån udlån1 : udlånList)
-        {
-        System.out.println(udlån1.toString());
-        }*/
-
-
-        Dialog dialog;
-
-        dialog = new DanskDialog();
-        System.out.println(dialog.hello());
-        System.out.println(dialog.welcome());
-        System.out.println(Input.getInt(dialog.askAge()));
-
-        dialog = new EngelskDialog();
-        System.out.println(dialog.hello());
-        System.out.println(dialog.welcome());
+        /*Dialog dialog;
 
 
         dialog = new DanskDialog();
@@ -67,8 +32,11 @@ public class Main
                 }
             }
 
-        }
+        }*/
 
 
+        Menu menu = new Menu();
+        menu.run();
+        
     }
 }
---
+++ src/Menu/Menu.java
@@ -0,0 +1,74 @@
+package Menu;
+
+import Language.DanskDialog;
+import Language.Dialog;
+import Language.EngelskDialog;
+
+import java.util.Scanner;
+
+public class Menu
+{
+    private Scanner scanner = new Scanner(System.in);
+    private Dialog dialog;
+
+    public void run()
+    {
+        selectLanguage();
+    }
+
+    private void selectLanguage()
+    {
+        System.out.println("Choose language:");
+        System.out.println("1. Dansk");
+        System.out.println("2. Engelsk");
+        System.out.println("3. Exit");
+        System.out.print("Choice: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                dialog = new DanskDialog();
+                break;
+            case 2:
+                dialog = new EngelskDialog();
+                break;
+            case 3:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Ugyldigt valg");
+                break;
+        }
+    }
+
+
+
+    private void printMenu()
+    {
+        System.out.println("1. Hent alle bøger");
+        System.out.println("2. Hent alle brugere");
+        System.out.println("3. Hent alle forfattere");
+        System.out.println("4. Hent alle postnumre");
+        System.out.println("5. Hent alle udlån");
+        System.out.println("6. Hent alle udlån for en bruger");
+        System.out.println("7. Hent alle udlån for en bog");
+        System.out.println("8. Hent alle udlån for en forfatter");
+        System.out.println("9. Hent alle udlån for et postnummer");
+        System.out.println("10. Hent alle udlån for en forfatter og et postnummer");
+        System.out.println("11. Hent alle udlån for en forfatter og en bog");
+        System.out.println("12. Hent alle udlån for en forfatter og en bruger");
+        System.out.println("13. Hent alle udlån for en forfatter og et postnummer");
+        System.out.println("14. Hent alle udlån for en bog og en bruger");
+        System.out.println("15. Hent alle udlån for en bog og et postnummer");
+        System.out.println("16. Hent alle udlån for en bruger og et postnummer");
+        System.out.println("17. Hent alle udlån for en forfatter, en bog og en bruger");
+        System.out.println("18. Hent alle udlån for en forfatter, en bog og et postnummer");
+        System.out.println("19. Hent alle udlån for en forfatter, en bruger og et postnummer");
+        System.out.println("20. Hent alle udlån for en bog, en bruger og et postnummer");
+        System.out.println("21. Hent alle udlån for en forfatter, en bog, en bruger og et postnummer");
+        System.out.println("22. Hent alle udlån for en forfatter, en bog, en bruger og et postnummer");
+        System.out.println("23. Hent alle udlån for en forfatter, en bog, en bruger og et postnummer");
+    }
+}
